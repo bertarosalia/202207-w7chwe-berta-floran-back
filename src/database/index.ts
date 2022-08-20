83a0960c5f5f14460a5541d1117189526c5701f4
Berta Floran <bertafloran@hotmail.com>
2022-08-20 15:44:30 +0200

    install correct version chalk and change imports

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,24 +1,24 @@
-import "../../loadEnvironment";
+import "../loadEnvironment";
 import Debug from "debug";
-import chalk from "chalk";
 import mongoose from "mongoose";
+import chalk from "chalk";
 
 const debug = Debug("users:database:index");
 
 const connectDB = (mongoUrl: string) =>
   new Promise((resolve, reject) => {
-    mongoose.set("toJSON", {
-      virtuals: true,
-      transform: (doc, ret) => {
-        const newDocument = { ...ret };
-        // eslint-disable-next-line no-underscore-dangle
-        delete newDocument._v;
-        // eslint-disable-next-line no-underscore-dangle
-        delete newDocument._id;
-        delete newDocument.passwd;
-        return newDocument;
-      },
-    });
+    // mongoose.set("toJSON", {
+    //   virtuals: true,
+    //   transform: (doc, ret) => {
+    //     const newDocument = { ...ret };
+    //     // eslint-disable-next-line no-underscore-dangle
+    //     delete newDocument._v;
+    //     // eslint-disable-next-line no-underscore-dangle
+    //     delete newDocument._id;
+    //     delete newDocument.passwd;
+    //     return newDocument;
+    //   },
+    // });
     mongoose.connect(mongoUrl, (error) => {
       if (error) {
         debug(chalk.red("Error connecting to Database", error.message));
