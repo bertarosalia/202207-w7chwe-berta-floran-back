dcc3bb20dc02b1c3418db28aa60bfc477776f4e3
Berta Floran <bertafloran@hotmail.com>
2022-08-20 11:43:33 +0200

    install morgan types

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import cors from "cors";
 import morgan from "morgan";
+import cors from "cors";
 import usersRouter from "./routers/usersRouters";
 
 const app = express();
