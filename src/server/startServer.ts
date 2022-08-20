b646b58e8c9eec013a3db47658c66ef593fb9e4e
Berta Floran <bertafloran@hotmail.com>
2022-08-20 13:03:18 +0200

    typed error in startServer

--- src/server/startServer.ts
+++ src/server/startServer.ts
@@ -1,6 +1,7 @@
 import Debug from "debug";
 import chalk from "chalk";
 import app from ".";
+import CustomError from "./utils/Custom Error";
 
 const debug = Debug("users:server:startServer");
 
@@ -10,7 +11,7 @@ const startServer = (port: number) =>
       debug(chalk.green(`Server listening on http://localhost:${port}`));
       resolve(true);
 
-      server.on("error", (error) => {
+      server.on("error", (error: CustomError) => {
         debug(chalk.red("Error starting the server"));
         if (error.code === "EADDRINUSE") {
           debug(chalk.red(`Port ${port} in use`));
