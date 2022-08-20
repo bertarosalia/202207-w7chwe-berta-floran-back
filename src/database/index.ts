68294eb6f47b682c90000a71167dd6ea2cbc0c4d
Berta Floran <bertafloran@hotmail.com>
2022-08-20 13:46:46 +0200

    solve code audit import

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,28 +3,31 @@ import Debug from "debug";
 import chalk from "chalk";
 import mongoose from "mongoose";
 
-const debug = Debug(users:database:index);
+const debug = Debug("users:database:index");
 
-export const connectDB = (mongoUrl: string) => new Promise ((resolve, reject) =>{
-  mongoose.set("toJSON", {
-    virtuals:true,
-    transform: (doc, ret) => {
-      const newDocument = {...ret};
-       // eslint-disable-next-line no-underscore-dangle
-       delete newDocument._v;
+const connectDB = (mongoUrl: string) =>
+  new Promise((resolve, reject) => {
+    mongoose.set("toJSON", {
+      virtuals: true,
+      transform: (doc, ret) => {
+        const newDocument = { ...ret };
         // eslint-disable-next-line no-underscore-dangle
-       delete newDocument._id;
-       delete newDocument.passwd;
-       return newDocument;
-    }
-  })
-  mongoose.connect(mongoUrl,(error)=> {
-    if(error){
-      debug(chalk.red("Error connecting to Database", error.message));
-      reject(error);
-      return;
+        delete newDocument._v;
+        // eslint-disable-next-line no-underscore-dangle
+        delete newDocument._id;
+        delete newDocument.passwd;
+        return newDocument;
+      },
+    });
+    mongoose.connect(mongoUrl, (error) => {
+      if (error) {
+        debug(chalk.red("Error connecting to Database", error.message));
+        reject(error);
+        return;
       }
       debug(chalk.green("Connected to Database"));
       resolve(true);
-  })
-})
+    });
+  });
+
+export default connectDB;
