0491b3ae648989442c7160f5ceb624d0552709d5
Berta Floran <bertafloran@hotmail.com>
2022-08-20 19:26:19 +0200

    create test for Custom Error

---
+++ src/server/utils/CustomError.test.ts
@@ -0,0 +1,22 @@
+import CustomError from "./Custom Error";
+
+describe("Given a custom error class", () => {
+  describe("When it´s instantianted with a statusCode 404, a public message and a private message ", () => {
+    test("It should create an object with those properties", () => {
+      const codeStatus = 400;
+      const privateMessage = "Endpoint not found";
+      const publicMessage = "Page not found";
+
+      const errorExpected = new Error() as CustomError;
+      errorExpected.StatusCode = codeStatus;
+      errorExpected.message = privateMessage;
+      errorExpected.publicMessage = publicMessage;
+
+      expect(errorExpected).toHaveProperty("StatusCode", codeStatus);
+      expect(errorExpected).toHaveProperty("message", privateMessage);
+      expect(errorExpected).toHaveProperty("publicMessage", publicMessage);
+      // expect(errorExpected).toHaveProperty("message", privateMessage);
+      // expect(errorExpected).toHaveProperty("publicMessage", publicMessage);
+    });
+  });
+});
